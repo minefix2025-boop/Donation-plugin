b8459c9e955770d2cb631ae8853bff8f173958fa
minefix2025-boop <minefix2025@gmail.com>
2026-05-24 16:09:34 +0700

    Update Main.java

--- src/main/java/com/pokupka/Main.java
+++ src/main/java/com/pokupka/Main.java
@@ -1,1 +1,26 @@
+package com.pokupka;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private ApiClient apiClient;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        apiClient = new ApiClient(
+                getConfig().getString("api-url"),
+                getConfig().getString("api-key"),
+                getConfig().getString("server-id"),
+                this
+        );
+        getServer().getPluginManager().registerEvents(new PurchaseListener(apiClient), this);
+        getLogger().info("Pokupka enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Pokupka disabled");
+    }
+}
