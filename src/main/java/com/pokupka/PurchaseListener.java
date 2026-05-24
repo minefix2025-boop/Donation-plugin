4e88a48d6631d0a2bbc42c522657c3646d355049
minefix2025-boop <minefix2025@gmail.com>
2026-05-24 16:10:33 +0700

    Update PurchaseListener.java

--- src/main/java/com/pokupka/PurchaseListener.java
+++ src/main/java/com/pokupka/PurchaseListener.java
@@ -1,1 +1,12 @@
+package com.pokupka;
 
+import org.bukkit.event.Listener;
+
+public class PurchaseListener implements Listener {
+
+    private ApiClient apiClient;
+
+    public PurchaseListener(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+}
